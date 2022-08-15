53eba67866d73d6b605b7447e5bd4a5cf2d32d0f
Crisp-Links <100052863+Crisp-Links@users.noreply.github.com>
2022-08-15 09:43:36 +0000

    Update settings.py

--- myrent/settings.py
+++ myrent/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'myrent.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'static')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
